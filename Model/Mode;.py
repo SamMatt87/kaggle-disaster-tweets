5d8acae7588bfcfa1b0271e2dc34c60046ec0ca1
SamMatt87 <sam.matthews.77@gmail.com>
2021-09-28 15:57:08 +1000

    final datasets for model

--- Model/Mode;.py
+++ Model/Mode;.py
@@ -3,6 +3,7 @@ import nltk
 import re
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelBinarizer
 
 train=pd.read_csv('../nlp-getting-started/train.csv')
 test = pd.read_csv('../nlp-getting-started/test.csv')
@@ -73,8 +74,28 @@ CV = CountVectorizer()
 train_vectoriser = CV.fit_transform(train_set.clean_text.values)
 validation_vectoriser = CV.transform(validation_set.clean_text.values)
 HCV = CountVectorizer()
-test_hashtag_vectoriser = HCV.fit_transform(train_set.hashtags.values)
+train_hashtag_vectoriser = HCV.fit_transform(train_set.hashtags.values)
 validation_hashtag_vetoriser = HCV.transform(validation_set.hashtags.values)
-print(HCV.get_feature_names())
-
+def column_names(vectoriser, prefix):
+    columns = []
+    for column in vectoriser.get_feature_names():
+        columns.append(prefix+'_'+column)
+    return columns
+train_text_dense = pd.DataFrame(train_vectoriser.todense(),columns=column_names(CV,'text'))
+valid_text_dense = pd.DataFrame(validation_vectoriser.todense(),columns=column_names(CV,'text'))
+train_hash_dense = pd.DataFrame(train_hashtag_vectoriser.todense(),columns=column_names(HCV,'hash'))
+valid_hash_dense = pd.DataFrame(validation_hashtag_vetoriser.todense(),columns=column_names(HCV,'hash'))
+keyword_labeller = LabelBinarizer()
+train_keywords = keyword_labeller.fit_transform(train_set.keyword.astype(str))
+valid_keywords = keyword_labeller.transform(validation_set.keyword.astype(str))
+keyword_cols = []
+for keyword in keyword_labeller.classes_:
+    keyword_cols.append('keyword_'+keyword)
+train_keywords_df = pd.DataFrame(train_keywords, columns=keyword_cols)
+valid_keywords_df =pd.DataFrame(valid_keywords, columns=keyword_cols)
+train_filtered = train_set[['target', 'includes_website','num_hashtags', 'num_mentions', 'tweet_length']].reset_index()
+valid_filtered = validation_set[['target', 'includes_website','num_hashtags', 'num_mentions','tweet_length']].reset_index()
+train_full = pd.concat([train_filtered,train_text_dense, train_hash_dense, train_keywords_df], axis=1)
+valid_full = pd.concat([valid_filtered,valid_text_dense, valid_hash_dense, valid_keywords_df],axis=1)
+print(valid_full)
 
